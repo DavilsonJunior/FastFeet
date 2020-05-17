f45b9ae84380dee9ef49595bd487d02c7524d617
Davilson Junior <davilsonjr_jobs@yahoo.com>
2020-05-17 09:13:57 -0300

    creating Recipient Model and updating the settings for database connection

---
+++ src/app/models/Recipient.js
@@ -0,0 +1,28 @@
+import Sequelize, { Model } from 'sequelize';
+//import { Sequelize, Model } from 'sequelize';
+
+class Recipient extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        rua: Sequelize.STRING,
+        numero: Sequelize.INTEGER,
+        complemento: Sequelize.STRING,
+        estado: Sequelize.STRING,
+        cidade: Sequelize.STRING,
+        cep: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user'});
+  }
+}
+
+export default Recipient;
