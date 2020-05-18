1ac0da96675c7049d00d2cb1c9695467e502f8ff
Davilson Junior <davilsonjr_jobs@yahoo.com>
2020-05-18 01:01:13 -0300

    creating files upload

--- src/app/models/Recipient.js
+++ src/app/models/Recipient.js
@@ -1,5 +1,5 @@
-import Sequelize, { Model } from 'sequelize';
-//import { Sequelize, Model } from 'sequelize';
+//import Sequelize, { Model } from 'sequelize';
+import { Sequelize, Model } from 'sequelize';
 
 class Recipient extends Model {
   static init(sequelize) {
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import DestinatarioController from './app/controllers/DestinatarioController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
@@ -18,5 +22,7 @@ routes.use(authMiddleware);
 routes.get('/destinatarios', DestinatarioController.index);
 routes.post('/destinatarios', DestinatarioController.store);
 routes.put('/destinatarios/:id', DestinatarioController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
