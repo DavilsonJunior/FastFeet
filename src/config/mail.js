fbe416e2a04b8b3ff08762428bd760b64cc76381
Davilson Junior <davilsonjr_jobs@yahoo.com>
2020-05-20 22:47:12 -0300

    nodemailer, views for sends emails with .hbs, database redis for registers and Queue for became the sends email more performance

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '215a6f3856ac51',
+    pass: 'e77f1f137e1098',
+  },
+  default: {
+    from: 'Equipe FastFeet <noreply@fastfeet.com>',
+  }
+}
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dns: 'https://6eb1cc297802458388cb311932732e61@o388825.ingest.sentry.io/5248422',
+}
